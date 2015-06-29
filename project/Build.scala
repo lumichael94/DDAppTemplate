0e35b95d6243de8ce1a5bbf382e1a2e3a6df03ab
lumichael94 <lumichael94@gmail.com>
2015-06-28 22:58:28 -0400

    initial commit

---
+++ project/Build.scala
@@ -0,0 +1,24 @@
+import sbt._
+import Keys._
+import sbtassembly.Plugin._
+import AssemblyKeys._
+
+object CassandraExampleBuild extends Build {
+  lazy val buildSettings = Defaults.defaultSettings ++ Seq(
+    version := "1.0",
+    scalaVersion := "2.10.4"
+  )
+
+  lazy val app = Project(
+    "cassandra-example",
+    file("."),
+    settings = buildSettings ++ assemblySettings ++ Seq(
+      libraryDependencies ++= Seq(
+        "com.datastax.spark" %% "spark-cassandra-connector" % "1.4.0-M1",
+        // spark will already be on classpath when using spark-submit.
+        // marked as provided, so that it isn't included in assembly.
+        "org.apache.spark" %% "spark-catalyst" % "1.4.0" % "provided"
+      )
+    )
+  )
+}
---
+++ src/main/scala/SimpleApp.scala
@@ -0,0 +1,22 @@
+import org.apache.spark._
+import com.datastax.spark.connector._
+
+/** Read from cassandra to make sure things are working.
+  * Assumes test.hello table already exists and has data, see cassandra-example.cql
+  */
+object HelloWorldExample {
+  def main(args: Array[String]): Unit = {
+    // only setting app name, all other properties will be specified at runtime for flexibility
+    val conf = new SparkConf().setAppName("cassandra-example-hello")
+
+    val sc = new SparkContext(conf)
+
+    val hello = sc.cassandraTable[(String, String)]("test", "hello")
+
+    val first = hello.first
+
+    sc.stop
+
+    println(first)
+  }
+}
